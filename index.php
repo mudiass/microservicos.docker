58a7dbf74b42c84d530f7ecad797c6237ff409f6
mudiass <murillo_diastrabalho@outlook.com>
2026-02-02 09:55:16 -0300

    Atualização do index.php

--- index.php
+++ index.php
@@ -1,48 +1,52 @@
-<html>
-
-<head>
-<title>Exemplo PHP</title>
-</head>
-<body>
 
 <?php
 ini_set("display_errors", 1);
-header('Content-Type: text/html; charset=iso-8859-1');
 
-
-
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
-
-$servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
-
+error_reporting(E_ALL);
 // Criar conexão
+header('Content-Type: text/html; charset=UTF-8');
 
 
-$link = new mysqli($servername, $username, $password, $database);
+$servername = getenv('DB_HOST');
+$username   = getenv('DB_USER');
+$password   = getenv('DB_PASSWORD');
+$database   = getenv('DB_NAME');
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
 }
 
-$valor_rand1 =  rand(1, 999);
-$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
-$host_name = gethostname();
+$alunoId = rand(1, 999);
+$nome = strtoupper(substr(bin2hex(random_bytes(4)), 0, 8));
+$host = gethostname();
 
+// Prepared Statement (segurança)
+$stmt = $conn->prepare(
+    "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host)
+     VALUES (?, ?, ?, ?, ?, ?)"
+);
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$stmt->bind_param(
+    "isssss",
+    $alunoId,
+    $nome,
+    $nome,
+    $nome,
+    $nome,
+    $host
+);
 
-
-if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
+if ($stmt->execute()) {
+    echo "Registro criado com sucesso 🚀<br>";
+    echo "Servidor: " . $host;
 } else {
-  echo "Error: " . $link->error;
+     echo "Erro ao inserir: " . $stmt->error;
 }
 
-?>
-</body>
-</html>
+$stmt->close();
+$conn->close();
+
+echo "<br>Versão do PHP: " . phpversion();
